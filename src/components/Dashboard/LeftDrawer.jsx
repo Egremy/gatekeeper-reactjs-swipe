60002d7c7ec664ecb785b4aa49c3e55c2632f001
Leonel Ortega Fabian <leonel@topengineer.io>
2019-08-29 20:22:50 -0500

    Child integration

--- src/components/Dashboard/LeftDrawer.jsx
+++ src/components/Dashboard/LeftDrawer.jsx
@@ -6,23 +6,54 @@ import clsx from 'clsx';
 // Material-UI Core
 import Drawer from '@material-ui/core/Drawer';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import List from '@material-ui/core/List';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
 
 // Constants
+import { ListItem } from '@material-ui/core';
 import { GATEKEEPER_DRAWER_OPTIONS } from './constants';
 
+const myStyles = makeStyles(theme => ({
+  expansionPanelSummary: {
+    minHeight: 0,
+    margin: 0,
+    marginRight: -12,
+    '&.Mui-expanded': {
+      minHeight: 0,
+      margin: 0,
+      marginRight: -12,
+    },
+  },
+  expansionPanelDetails: {
+    display: 'block',
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: 12,
+  },
+  listItemIcon: {
+    minWidth: 0,
+  },
+  labelBold: {
+    fontWeight: 'bold',
+  }
+}));
+
 export const LeftDrawer = ({ classes, open }) => {
+  const myClasses = myStyles();
   const [state, setState] = React.useState({
     checkedA: true,
     checkedB: true,
@@ -80,27 +111,39 @@ export const LeftDrawer = ({ classes, open }) => {
       }}
       open={open}
     >
-      <Grid container>
-        <Grid item xs={12}>
-          FILTER
-        </Grid>
-        <Grid item xs={12}>
-          <Paper>
-            <InputBase
-              className={classes.input}
-              placeholder="Type to search"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-            <IconButton className={classes.iconButton} aria-label="search">
-              <SearchIcon />
-            </IconButton>
-          </Paper>
-        </Grid>
-      </Grid>
       <List>
+        <Paper square>
+          <ListItem>
+            <ListItemText>FILTER</ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemText>
+              <Paper square>
+                <InputBase
+                  fullWidth
+                  className={classes.input}
+                  placeholder="Type to search"
+                  inputProps={{ 'aria-label': 'search' }}
+                  endAdornment={(
+                    <IconButton className={classes.iconButton} aria-label="search">
+                      <SearchIcon />
+                    </IconButton>
+                  )}
+                />
+              </Paper>
+            </ListItemText>
+          </ListItem>
+        </Paper>
+      </List>
+      <List disablePadding>
         {createTwidgetsFromData(GATEKEEPER_DRAWER_OPTIONS).map((item) => (
-          <ExpansionPanel>
+          <ExpansionPanel square>
             <ExpansionPanelSummary
+              classes={{
+                root: myClasses.expansionPanelSummary,
+                content: myClasses.expansionPanelSummary,
+                expanded: myClasses.expansionPanelSummary,
+              }}
               expandIcon={<ExpandMoreIcon />}
               aria-controls="panel1a-content"
               id="panel1a-header"
@@ -111,17 +154,37 @@ export const LeftDrawer = ({ classes, open }) => {
                     checked={item.checked}
                     onChange={handleChange(item)}
                     value="checkedF"
-                    indeterminate
+                    indeterminate={item.indeterminate}
                   />
                 )}
                 label={item.title}
+                classes={{ label: myClasses.labelBold }}
               />
             </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                sit amet blandit leo lobortis eget.
-              </Typography>
+            <ExpansionPanelDetails classes={{ root: myClasses.expansionPanelDetails }}>
+              <List dense disablePadding>
+                {item.items.map((cb) => (
+                  <ListItem divider disableGutters>
+                    <ListItemIcon classes={{ root: myClasses.listItemIcon }}>
+                      <Checkbox
+                        checked={cb.checked}
+                        onChange={handleChange(cb)}
+                        value="checkedF"
+                        indeterminate={cb.indeterminate}
+                      />
+                    </ListItemIcon>
+                    <ListItemText primary={cb.title} />
+                    <Typography>{cb.count}</Typography>
+                    {cb.items.length > 0 ? (
+                      <ListItemSecondaryAction>
+                        <IconButton edge="end" aria-label="comments">
+                          {cb.items.collapsed ? <ExpandLess /> : <ExpandMoreIcon />}
+                        </IconButton>
+                      </ListItemSecondaryAction>
+                    ) : ''}
+                  </ListItem>
+                ))}
+              </List>
             </ExpansionPanelDetails>
           </ExpansionPanel>
         )
