9fa3c507ff3a28db05a732e196ba657a15b7561d
Leonel Ortega Fabian <leonel@topengineer.io>
2019-09-04 12:44:36 -0500

    Button image warning removed

--- src/components/Dashboard/utils/index.js
+++ src/components/Dashboard/utils/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 // Material-UI Icons
 import AlertsIcon from '@material-ui/icons/Error';
 
-import { IconButton } from '@material-ui/core';
 import Athena from '../../../static/assets/icons/Athena.svg';
 import CartAnalytics from '../../../static/assets/icons/CartAnalytics.svg';
 import Chart from '../../../static/assets/icons/Chart.svg';
@@ -21,69 +20,47 @@ export const iconToShow = (text) => {
   switch (text) {
     case 'Athena':
       return (
-        <IconButton>
-          <img src={Athena} alt={text} style={{ width: '24pt' }} />
-        </IconButton>
+        <img src={Athena} alt={text} style={{ width: '24pt' }} />
       );
     case 'CartAnalytics':
       return (
-        <IconButton>
-          <img src={CartAnalytics} alt={text} style={{ width: '24pt' }} />
-        </IconButton>
+        <img src={CartAnalytics} alt={text} style={{ width: '24pt' }} />
       );
     case 'Chart':
       return (
-        <IconButton>
-          <img src={Chart} alt={text} style={{ width: '24pt' }} />
-        </IconButton>
+        <img src={Chart} alt={text} style={{ width: '24pt' }} />
       );
     case 'Dashboard':
       return (
-        <IconButton>
-          <img src={Dashboard} alt={text} style={{ width: '24pt' }} />
-        </IconButton>
+        <img src={Dashboard} alt={text} style={{ width: '24pt' }} />
       );
     case 'Data':
       return (
-        <IconButton>
-          <img src={Data} alt={text} style={{ width: '24pt' }} />
-        </IconButton>
+        <img src={Data} alt={text} style={{ width: '24pt' }} />
       );
     case 'Map':
       return (
-        <IconButton>
-          <img src={Map} alt={text} style={{ width: '24pt' }} />
-        </IconButton>
+        <img src={Map} alt={text} style={{ width: '24pt' }} />
       );
     case 'Password':
       return (
-        <IconButton>
-          <img src={Password} alt={text} style={{ width: '24pt' }} />
-        </IconButton>
+        <img src={Password} alt={text} style={{ width: '24pt' }} />
       );
     case 'SystemHealth':
       return (
-        <IconButton>
-          <img src={SystemHealth} alt={text} style={{ width: '24pt' }} />
-        </IconButton>
+        <img src={SystemHealth} alt={text} style={{ width: '24pt' }} />
       );
     case 'User':
       return (
-        <IconButton>
-          <img src={User} alt={text} style={{ width: '24pt' }} />
-        </IconButton>
+        <img src={User} alt={text} style={{ width: '24pt' }} />
       );
     case 'WheelEvents':
       return (
-        <IconButton>
-          <img src={WheelEvents} alt={text} style={{ width: '24pt' }} />
-        </IconButton>
+        <img src={WheelEvents} alt={text} style={{ width: '24pt' }} />
       );
     case 'Wheels':
       return (
-        <IconButton>
-          <img src={Wheels} alt={text} style={{ width: '24pt' }} />
-        </IconButton>
+        <img src={Wheels} alt={text} style={{ width: '24pt' }} />
       );
     default:
       return <AlertsIcon />;
