b798fe7968dbcc4beb566298f53ad065b970ea87
Egremy <egremy.v@gmail.com>
2019-09-06 12:29:59 -0500

    Update GraphComponent.jsx
    New (demo) Graph

--- src/components/Dashboard/GraphComponent.jsx
+++ src/components/Dashboard/GraphComponent.jsx
@@ -1,23 +1,24 @@
 // Dependencies
-import React from 'react';
+import React, { iframe } from 'react';
 import PropTypes from 'prop-types';
 
 export const GraphComponent = ({ classes }) => (
   <div className={classes.frames}>
-    <img
-      style={{
-        '-webkit-backface-visibility': 'hidden',
-        '-webkit-perspective': 1000,
-        display: 'block',
-        height: 'auto',
-        pointerEvents: 'none',
-        width: '100%'
-      }}
-      src="https://rockcreekpizzaco.com/wp-content/uploads/2019/04/bd68ef5082a0e59e346a586f12ff520d.png"
-      alt="graph"
+    <iframe
+      title="Chart"
+      width="100%"
+      height="100%"
+      src="https://datastudio.google.com/embed/reporting/10BpUDqyV2adigMPaF4w735kwEqtGaEU8/page/DjD"
+      frameBorder="0"
+      style={iframeStyle}
+      allowFullScreen
     />
   </div>
 );
+
+const iframeStyle = {
+  border: 0
+};
 
 GraphComponent.propTypes = {
   classes: PropTypes.objectOf(PropTypes.any),
